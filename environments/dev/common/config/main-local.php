6ef1071d92534edf5a7cd7e5f0969328962f8957
Clark <C18666211369@outlook.com>
2019-09-20 18:44:30 +0800

    配置mysqlDemo数据库

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -8,6 +8,13 @@ return [
             'password' => 'root',
             'charset' => 'utf8',
         ],
+        'mysqlDemo' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=mysql_demo',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',

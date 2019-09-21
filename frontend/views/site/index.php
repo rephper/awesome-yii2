225a18309d3bbff61dddc3939007e64ad824c766
Clark <C18666211369@outlook.com>
2019-09-21 18:08:19 +0800

    修改首页

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,50 +2,50 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'demo.Yii2项目 前台';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">PHP后端开发笔记</a></p>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p class="lead">老司机 不迷路 全靠知识地图</p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-lg-3">
+                <h2>参考资源</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://www.yiiframework.com/doc/guide/2.0/zh-cn">Yii 2.0 权威指南</a></p>
+                <p><a class="btn btn-default" href="https://www.php.net/manual/zh/">PHP手册</a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-lg-3">
+                <h2>PHP7</h2>
+                <p><a class="btn btn-default" href="/">PHP7新特性</a></p>
+                <p><a class="btn btn-default" href="/">语言参考</a></p>
+                <p><a class="btn btn-default" href="/">安全</a></p>
+                <p><a class="btn btn-default" href="/">特点</a></p>
+                <p><a class="btn btn-default" href="/">函数参考</a></p>
+                <p><a class="btn btn-default" href="/">PHP 核心：骇客指南</a></p>
+                <p><a class="btn btn-default" href="/">踩坑</a></p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-lg-3">
+                <h2>MySQL</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p><a class="btn btn-default" href="/">基本概念</a></p>
+                <p><a class="btn btn-default" href="/">数据类型</a></p>
+                <p><a class="btn btn-default" href="/">DCL语句（权限管理）</a></p>
+                <p><a class="btn btn-default" href="/">DDL语句（结构定义）</a></p>
+                <p><a class="btn btn-default" href="/">DML语句（数据操作）</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h2>Yii2最佳实践</h2>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="/">结构</a></p>
+                <p><a class="btn btn-default" href="/">生命周期</a></p>
             </div>
         </div>
 
